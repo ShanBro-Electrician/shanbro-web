f6788dcca423631f4c92767594c79dfe9636c6b8
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-11 21:36:45 +0800

    Configured default sans font to be Nunito_Sans from Google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans  = Nunito_Sans({ subsets: ["latin"],
+  variable: "--font-sans", });
 
 export const metadata: Metadata = {
   title: "ShanBro | Best Electrical and Plumbing Services",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>{children}</body>
     </html>
   );
 }
