cc99b1f375c741dd2a015793d1ba2e16bcd2acf7
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-16 23:34:02 +0800

    Reviews section complete mobile and desktop responsive

--- components/sections/reviews.tsx
+++ components/sections/reviews.tsx
@@ -1,35 +1,66 @@
+import { cn } from "@/lib/utils";
 import { Star } from "lucide-react";
+
+const reviews = [
+  {
+    name: "Ashley Foo",
+    text: "ShanBro Electrical Service is the best electrician I've searched for. Everyone give 5 star for a reason! He came to my company for electric trip issue, and he found the cause of electric trip very fast too. Price is reasonable, and he can also give estimation cost after telling the issue we are facing before we book the service with him. Thank you ShanBro for your service!",
+  },
+  {
+    name: "Richelle",
+    text: "Recommended!! Happy with their works!! Always responsible, craftsmanship is neat and tidy, price is reasonable also. Have been approaching them for the 3rd times already for different scopes, water plumbing, tiles works and electrical wiring.👍🏻👍🏻",
+  },
+  {
+    name: "Sara Roy",
+    text: "Found their ads on facebook. happy and satisfied with their services. urgently need wireman and he came immediately when other wiremen ignored me and didn't reply my text. he did the job very well and quickly.affordable charges. kerja bguss! my husband and i sukaa sangat",
+  },
+];
 
 const Reviews = () => {
   return (
-    <div className="container bg-neutral-200 py-8 space-y-4">
-      <div className="flex flex-col-reverse text-center font-display">
-        <h1 className="font-bold text-2xl">WHAT OUR CUSTOMERS SAY ABOUT US</h1>
-        <h2 className="text-muted-foreground">REVIEWS</h2>
-      </div>
-      <div className="space-y-2">
-        <ReviewCard
-          name="Ashley Foo"
-          text="ShanBro Electrical Service is the best electrician I've searched for. Everyone give 5 star for a reason! He came to my company for electric trip issue, and he found the cause of electric trip very fast too. Price is reasonable, and he can also give estimation cost after telling the issue we are facing before we book the service with him. Thank you ShanBro for your service!"
-        />
-        <ReviewCard
-          name="Richelle"
-          text="Recommended!! Happy with their works!!
-Always responsible, craftsmanship is neat and tidy, price is reasonable also.
-Have been approaching them for the 3rd times already for different scopes, water plumbing, tiles works and electrical wiring.👍🏻👍🏻"
-        />
-        <ReviewCard
-          name="Sara Roy"
-          text="Found their ads on facebook. happy and satisfied with their services. urgently need wireman and he came immediately when other wiremen ignored me and didn't reply my text. he did the job very well and quickly.affordable charges. kerja bguss! my husband and i sukaa sangat"
-        />
+    <div className="bg-neutral-200">
+      <div className="container py-8 space-y-4">
+        <div className="flex flex-col-reverse text-center font-display">
+          <h1 className="font-bold text-2xl">
+            WHAT OUR CUSTOMERS SAY ABOUT US
+          </h1>
+          <h2 className="text-muted-foreground">REVIEWS</h2>
+        </div>
+        <div className="space-y-2 md:space-y-0 md:grid md:grid-cols-4 md:gap-8 md:items-end md:py-8 md:px-10 lg:px-20 2xl:px-48">
+          <ReviewCard
+            name={reviews[0].name}
+            text={reviews[0].text}
+            className="col-span-2"
+          />
+          <ReviewCard
+            name={reviews[1].name}
+            text={reviews[1].text}
+            className="col-span-2 md:h-max"
+          />
+          <ReviewCard
+            name={reviews[2].name}
+            text={reviews[2].text}
+            className="col-start-2 col-span-2"
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-export const ReviewCard = ({ name, text }: { name: string; text: string }) => {
+interface ReviewCardProps extends React.ComponentPropsWithoutRef<"div"> {
+  name: string;
+  text: string;
+}
+
+export const ReviewCard = ({
+  name,
+  text,
+  className,
+  ...rest
+}: ReviewCardProps) => {
   return (
-    <div className="bg-white p-4 space-y-2">
+    <div className={cn("bg-white p-4 space-y-2", className)} {...rest}>
       <h3 className="font-bold text-xl">{name}</h3>
       <div className="flex">
         {[...Array(5)].map((_, i) => (
