508c27dfd9463bd0385fe774a468e59289f8b187
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-20 16:19:51 +0800

    Hero redesign

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-r from-black/95 via-black/80 to-black/25 relative">
+    <div className="bg-black/70 relative rounded-b-3xl sm:rounded-b-[50px] overflow-hidden">
       {/* Background Image */}
-      <div className="absolute top-0 bottom-0 left-0 right-0 -z-10">
+      <div className="absolute top-0 bottom-0 left-0 right-0 -z-10 blur-sm">
         <Image
           src="/HeroBg.jpg"
           fill
@@ -16,14 +16,16 @@ const Hero = () => {
         />
       </div>
       {/* Content */}
-      <div className="container space-y-8 flex flex-col pt-20 pb-40 sm:pl-20">
-        <h1 className="text-white font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-          <span className="font-normal text-2xl">WE EXCEL IN</span>
+      <div className="container space-y-8 flex flex-col py-36 sm:pl-20">
+        <h1 className="text-lime-300 font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          <span className="font-normal text-lg text-neutral-300">
+            WE EXCEL IN
+          </span>
           <br />
           <span>ELECTRICAL</span> &<br />
           <span>PLUMBING</span>
         </h1>
-        <p className="text-xs text-muted/70 max-w-96">
+        <p className="text-xs text-neutral-300 max-w-96">
           At ShanBro, we’ve been serving customers with high quality repairs,
           installations, and emergency services. With transparent pricing and a
           commitment to safety, we’re your trusted partner for all your
