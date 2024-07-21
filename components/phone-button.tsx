0d175932c8dd9163182ce822bc1b1b463ea171ca
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-21 13:04:33 +0800

    Updated gallery images, Updated phone numbers

--- components/phone-button.tsx
+++ components/phone-button.tsx
@@ -3,9 +3,13 @@ import React from "react";
 
 const PhoneButton = () => {
   return (
-    <a className="rounded-full bg-lime-200 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1">
+    <a
+      href="tel:+60168566184"
+      target="_blank"
+      className="rounded-full bg-lime-200 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
+    >
       {/* <Phone height={20} width={20} /> */}
-      +6012-345-6789
+      +6016-856-6184
     </a>
   );
 };
--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const WhatsappButton = () => {
   return (
-    <a className="rounded-full bg-lime-950 text-white py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1">
+    <a
+      href="https://wa.me/60168566184"
+      target="_blank"
+      className="rounded-full bg-lime-950 text-white py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
+    >
       <img src="WhatsApp.svg" alt="whatsapp logo" width={20} />
       Whatsapp
     </a>
