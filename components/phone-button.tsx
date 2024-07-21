4753d176a14bbdf344723e8a2da5d47962668332
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-21 14:39:21 +0800

    Changed cta colors

--- components/phone-button.tsx
+++ components/phone-button.tsx
@@ -6,7 +6,7 @@ const PhoneButton = () => {
     <a
       href="tel:+60168566184"
       target="_blank"
-      className="rounded-full bg-lime-200 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
+      className="rounded-full bg-lime-200/80 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
     >
       {/* <Phone height={20} width={20} /> */}
       +6016-856-6184
