2a150387c62b0373b7831d61c8f1113dc6f83a0c
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-21 17:14:47 +0800

    Animated reviews header

--- components/sections/reviews.tsx
+++ components/sections/reviews.tsx
@@ -1,29 +1,29 @@
 import { cn } from "@/lib/utils";
 import { Star } from "lucide-react";
-
-const reviews = [
-  {
-    name: "Ashley Foo",
-    text: "ShanBro Electrical Service is the best electrician I've searched for. Everyone give 5 star for a reason! He came to my company for electric trip issue, and he found the cause of electric trip very fast too. Price is reasonable, and he can also give estimation cost after telling the issue we are facing before we book the service with him. Thank you ShanBro for your service!",
-  },
-  {
-    name: "Richelle",
-    text: "Recommended!! Happy with their works!! Always responsible, craftsmanship is neat and tidy, price is reasonable also. Have been approaching them for the 3rd times already for different scopes, water plumbing, tiles works and electrical wiring.👍🏻👍🏻",
-  },
-  {
-    name: "Sara Roy",
-    text: "Found their ads on facebook. happy and satisfied with their services. urgently need wireman and he came immediately when other wiremen ignored me and didn't reply my text. he did the job very well and quickly.affordable charges. kerja bguss! my husband and i sukaa sangat",
-  },
-];
+import { motion } from "framer-motion";
 
 const Reviews = () => {
   return (
     <div className="container py-20 space-y-4">
       <div className="flex flex-col-reverse text-center gap-2">
-        <h1 className="text-xl text-lime-100 font-semibold leading-none md:text-3xl md:w-[30rem] md:mx-auto">
+        <motion.h1
+          initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
+          transition={{ duration: 0.3 }}
+          whileInView={{ opacity: 1, y: 0, filter: "blur(0)" }}
+          viewport={{ margin: "-80px" }}
+          className="text-xl text-lime-100 font-semibold leading-none md:text-3xl md:w-[30rem] md:mx-auto"
+        >
           WHAT OUR CUSTOMERS SAY ABOUT US
-        </h1>
-        <h2 className="text-muted-foreground">REVIEWS</h2>
+        </motion.h1>
+        <motion.h2
+          initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
+          transition={{ duration: 0.4 }}
+          whileInView={{ opacity: 1, y: 0, filter: "blur(0)" }}
+          viewport={{ margin: "-80px" }}
+          className="text-muted-foreground"
+        >
+          REVIEWS
+        </motion.h2>
       </div>
       <div className="space-y-2 md:space-y-0 md:grid md:grid-cols-4 md:gap-8 md:items-end md:py-8 md:px-10 lg:px-20 2xl:px-48">
         <ReviewCard
@@ -62,12 +62,27 @@ export const ReviewCard = ({
       <h3 className="text-xs font-semibold text-neutral-200">{name}</h3>
       <div className="flex">
         {[...Array(5)].map((_, i) => (
-          <Star key={i} stroke="0" width={16} fill="#FFC700" />
+          <Star key={i} stroke="0" width={16} className="fill-lime-500" />
         ))}
       </div>
       <p className="text-xs text-neutral-400 leading-normal">{text}</p>
     </div>
   );
 };
+
+const reviews = [
+  {
+    name: "Ashley Foo",
+    text: "ShanBro Electrical Service is the best electrician I've searched for. Everyone give 5 star for a reason! He came to my company for electric trip issue, and he found the cause of electric trip very fast too. Price is reasonable, and he can also give estimation cost after telling the issue we are facing before we book the service with him. Thank you ShanBro for your service!",
+  },
+  {
+    name: "Richelle",
+    text: "Recommended!! Happy with their works!! Always responsible, craftsmanship is neat and tidy, price is reasonable also. Have been approaching them for the 3rd times already for different scopes, water plumbing, tiles works and electrical wiring.👍🏻👍🏻",
+  },
+  {
+    name: "Sara Roy",
+    text: "Found their ads on facebook. happy and satisfied with their services. urgently need wireman and he came immediately when other wiremen ignored me and didn't reply my text. he did the job very well and quickly.affordable charges. kerja bguss! my husband and i sukaa sangat",
+  },
+];
 
 export default Reviews;
