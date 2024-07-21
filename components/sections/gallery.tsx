819073964aa00d90e046c3824482ffd5abace71e
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-21 21:35:32 +0800

    Gallery animations

--- components/sections/gallery.tsx
+++ components/sections/gallery.tsx
@@ -1,4 +1,49 @@
+import { motion, Variants } from "framer-motion";
 import Image from "next/image";
+
+const Gallery = () => {
+  const motionVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 30,
+      transformOrigin: "top",
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transformOrigin: "top",
+      transition: { duration: 0.3 },
+    },
+  };
+
+  return (
+    <div className="container py-8">
+      <motion.h1
+        viewport={{ margin: "-80px" }}
+        variants={motionVariants}
+        initial="hidden"
+        whileInView="visible"
+        className="text-xl md:text-3xl text-lime-100 font-semibold mb-2"
+      >
+        GALLERY
+      </motion.h1>
+      <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {images.map((img) => (
+          <motion.div
+            viewport={{ margin: "-100px", once: true }}
+            variants={motionVariants}
+            initial="hidden"
+            whileInView="visible"
+            key={img.src}
+            className="relative h-32 sm:h-48 md:h-72 lg:h-96"
+          >
+            <Image src={img.src} alt={img.alt} fill className="object-cover" />
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const images = [
   { src: "/gallery/1.jpg", alt: "Working with electrical plugs." },
@@ -16,22 +61,5 @@ const images = [
   { src: "/gallery/7.jpg", alt: "Man working on pipes." },
   { src: "/gallery/8.jpg", alt: "Working with more electrical plugs." },
 ];
-
-const Gallery = () => {
-  return (
-    <div className="container py-8">
-      <h1 className="text-xl md:text-3xl text-lime-100 font-semibold mb-2">
-        GALLERY
-      </h1>
-      <div className="grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {images.map((img) => (
-          <div key={img.src} className="relative h-32 sm:h-48 md:h-72 lg:h-96">
-            <Image src={img.src} alt={img.alt} fill className="object-cover" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
 
 export default Gallery;
