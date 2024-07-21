5dca932236cc7758d4578fa610468dd2760bff0e
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-21 16:19:51 +0800

    Animated border radius for dark sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
+"use client";
 import Hero from "@/components/sections/hero";
 import Services from "@/components/sections/services";
 import Reviews from "@/components/sections/reviews";
 import Gallery from "@/components/sections/gallery";
 import Footer from "@/components/sections/footer";
+import { motion, useScroll } from "framer-motion";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Services />
-      <div className="bg-gradient-to-br from-stone-900 to-black rounded-t-3xl sm:rounded-t-[80px]">
+      <motion.div
+        // rounded-t-3xl sm:rounded-t-[80px]
+        className="bg-gradient-to-br from-stone-900 to-black"
+        initial={{ borderTopLeftRadius: 200, borderTopRightRadius: 200 }}
+        whileInView={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+        viewport={{ margin: "-80px" }}
+      >
         <Reviews />
         <Gallery />
         <div className="p-4">
           <Footer />
         </div>
-      </div>
+      </motion.div>
     </main>
   );
 }
