d50e7ce49b172d0dd39850bddaf8955a2e7d2e52
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-25 15:41:08 +0800

    Default font urbanist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Urbanist as Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar";
