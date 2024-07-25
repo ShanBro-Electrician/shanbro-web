c4bcf54672ec0a2b3d24c602d0dd623814b7695b
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-25 20:56:39 +0800

    Back to inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Urbanist as Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar";
