2642f15982b9e913eda4c2733430c72f41707847
Sheikh Ameen <sheikhameen123@gmail.com>
2024-07-25 20:55:54 +0800

    Very functional navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,94 @@
-import Image from "next/image";
+"use client";
 import PhoneButton from "./phone-button";
 import WhatsappButton from "./whatsapp-button";
+import Logo from "./logo";
+import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isSolid, setIsSolid] = useState(false);
+
+  const onScroll = () => {
+    if (window.scrollY > 150) {
+      setIsSolid(true);
+    } else {
+      setIsSolid(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  });
+
+  const menuVariants: Variants = {
+    open: {
+      scaleY: 1,
+      transition: { staggerChildren: 0.2, delayChildren: 0.2 },
+    },
+    closed: {
+      scaleY: 0,
+      transition: {
+        staggerChildren: 0.2,
+        when: "afterChildren",
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const itemVariants: Variants = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: 10 },
+  };
+
   return (
-    <div className="z-50 fixed top-0 right-0 left-0 sm:py-4 sm:px-4">
-      <div className="shadow-lg bg-zinc-100 flex justify-between items-center p-2 sm:rounded-full sm:w-max sm:mx-auto sm:gap-32">
-        {/* shadow-lg bg-gradient-to-r from-lime-200 to-lime-400 container flex justify-between py-2 sm:rounded-full sm:p-2 */}
-        <div className="relative h-12 w-40 ml-4">
-          <Image src="/Logo.svg" alt="Logo" fill />
-        </div>
-        <div className="flex flex-col gap-1 sm:flex-row">
+    <div
+      className={cn(
+        "fixed z-50 w-full transition-colors duration-500",
+        isOpen || isSolid
+          ? "text-zinc-900 bg-white"
+          : "text-zinc-100 bg-transparent"
+      )}
+    >
+      <div className="border-b-[1px] border-b-stone-100/30 container py-2 flex justify-between items-center">
+        <Logo height={48} />
+        <Menu className="sm:hidden" onClick={() => setIsOpen(!isOpen)} />
+        <div className="hidden sm:flex gap-2">
           <WhatsappButton />
           <PhoneButton />
         </div>
       </div>
+
+      {/* Mobile Nav screen */}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="sm:hidden h-96 flex flex-col p-4 justify-center items-center gap-2 bg-white origin-top"
+            animate="open"
+            initial="closed"
+            exit="closed"
+            variants={menuVariants}
+          >
+            <div className="text-zinc-950 font-semibold text-xl">
+              Contact Us
+            </div>
+            <motion.div variants={itemVariants}>
+              <WhatsappButton />
+            </motion.div>
+            <motion.div variants={itemVariants}>
+              <PhoneButton />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
+
+// {/* <div className="flex flex-col gap-1 sm:flex-row">
+//   <WhatsappButton />
+//   <PhoneButton />
+// </div> */}
 
 export default Navbar;
