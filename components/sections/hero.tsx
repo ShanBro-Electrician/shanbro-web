a29f5b232945121e121b8ae6f9b0d4d2d04772ef
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-06 12:53:40 +0800

    Added link to tiktok

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { Separator } from "../ui/separator";
 import { useRef } from "react";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
 
 const Hero = () => {
   const bgRef = useRef(null);
@@ -16,8 +18,8 @@ const Hero = () => {
   const bgScale = useTransform(scrollYProgress, [0, 1], [1, 1.3]);
 
   return (
-    <div className="relative bg-zinc-950/50">
-      {/* Background image */}
+    <div className="relative bg-zinc-950/60">
+      {/* Background images */}
       <div
         ref={bgRef}
         className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden pointer-events-none"
@@ -65,13 +67,31 @@ const Hero = () => {
             <PhoneButton />
           </div>
         </div>
-        <p className="text-sm text-stone-200">
+        <p className="text-base text-stone-300">
           At ShanBro, we&apos;ve been serving customers with high quality
           repairs, installations, and emergency services. With transparent
           pricing and a commitment to safety, we&apos;re your trusted partner
           for all your electrical and plumbing needs. Contact us today for a
           free quote!
         </p>
+        <ul className="self-end">
+          {[
+            {
+              site: "TikTok",
+              href: "https://www.tiktok.com/@shanbro.electrici",
+            },
+          ].map((s) => (
+            <li key={s.site}>
+              <Link
+                className="text-white text-3xl font-semibold gap-2 flex items-center justify-end"
+                href={s.href}
+                target="_blank"
+              >
+                {s.site} <ArrowUpRight width={40} height={40} />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </motion.div>
     </div>
   );
