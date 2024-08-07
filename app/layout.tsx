b85ffb121a9431493cfd220c1dc953c7d4f024a5
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-07 13:18:52 +0800

    Setup google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-N3CVV05G1Z" />
       <body
         className={cn(
           "min-h-screen bg-gradient-to-b from-zinc-50 to-zinc-200 font-sans antialiased",
