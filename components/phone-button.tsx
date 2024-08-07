5bb80e33f2349f8ac8aac753a7808a3747ed8409
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-07 12:48:20 +0800

    Update phone

--- components/phone-button.tsx
+++ components/phone-button.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const PhoneButton = () => {
   return (
     <a
-      href="tel:+60168566184"
+      href="tel:+60182709026"
       target="_blank"
       className="rounded-full bg-lime-200/80 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
     >
       {/* <Phone height={20} width={20} /> */}
-      +6016-856-6184
+      +6018-270 9026
     </a>
   );
 };
--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const WhatsappButton = () => {
   return (
     <a
-      href="https://wa.me/60168566184"
+      href="https://wa.me/60182709026"
       target="_blank"
       className="rounded-full bg-lime-600 text-white py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
     >
