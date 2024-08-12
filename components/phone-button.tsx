2d65ad9268dc88f6fad0b350ce1d5c539ee772f3
Sheikh Ameen <sheikhameen123@gmail.com>
2024-08-12 13:20:18 +0800

    More customizable and reusable action buttons

--- components/phone-button.tsx
+++ components/phone-button.tsx
@@ -1,12 +1,20 @@
+import { cn } from "@/lib/utils";
 import { Phone } from "lucide-react";
 import React from "react";
 
-const PhoneButton = () => {
+const PhoneButton = ({
+  className,
+  ...rest
+}: React.ComponentPropsWithoutRef<"a">) => {
   return (
     <a
       href="tel:+60182709026"
       target="_blank"
-      className="rounded-full bg-lime-200/80 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
+      className={cn(
+        "rounded-full bg-lime-200/80 text-lime-950 py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1",
+        className
+      )}
+      {...rest}
     >
       {/* <Phone height={20} width={20} /> */}
       +6018-270 9026
--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -1,12 +1,20 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
-const WhatsappButton = () => {
+const WhatsappButton = ({
+  className,
+  ...rest
+}: React.ComponentPropsWithoutRef<"a">) => {
   return (
     <a
       href="https://wa.me/60182709026"
       target="_blank"
-      className="rounded-full bg-lime-600 text-white py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1"
+      className={cn(
+        "rounded-full bg-lime-600 text-white py-2 px-4 cursor-pointer text-xs font-semibold flex items-center gap-1",
+        className
+      )}
+      {...rest}
     >
       <div className="relative w-6 h-6">
         <Image src="WhatsApp.svg" alt="whatsapp logo" fill />
